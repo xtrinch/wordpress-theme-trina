a5a4d937f98faedf92d075def21068b351239ea3
Mojca Rojko <mojca.rojko@student.uni-lj.si>
2017-06-24 14:10:36 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@ add_action('after_setup_theme', 'factoryreset_setup');
 function factoryreset_setup(){
 load_theme_textdomain('factoryreset', get_template_directory() . '/languages');
 add_theme_support( 'title-tag' );
+add_theme_support( 'custom-header', $args );
 add_theme_support( 'automatic-feed-links' );
 add_theme_support( 'post-thumbnails' );
 global $content_width;
