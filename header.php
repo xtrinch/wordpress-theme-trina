d48b92d7f03d6d1f1b6e24fdb9de5cc60a913132
Mojca Rojko <mojca.rojko@student.uni-lj.si>
2017-06-24 14:03:06 +0200

    Update header.php

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 <div class="header-container">
   <div id="branding">
   <div id="site-title">
-  <img src="<?php bloginfo('template_url'); ?>/headphones.svg" width=75 class="main-icon" />
+  <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/headphones.svg" width=75 class="main-icon" />
     <?php if ( false ) {} else {echo '<h1>';} ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo( 'name' ) ?>" rel="home"><?php bloginfo( 'name' ) ?></a><?php if ( false ) {} else {echo '</h1>';} ?>
   </div>
   <!--<p id="site-description"><?php bloginfo( 'description' ) ?></p>-->
