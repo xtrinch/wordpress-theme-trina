468d2c2b3c9db8f99a9ee96680bdcf512915b395
xTrinch <mojca.rojko@gmail.com>
2017-06-26 10:59:38 +0200

    Fonts fix

--- entry-meta.php
+++ entry-meta.php
@@ -6,7 +6,7 @@
   </div>
 <div>
 <span class="author vcard"><a class="url fn n" href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" title="<?php printf( __( 'View all articles by %s', 'trina' ), $authordata->display_name ); ?>"><?php the_author(); ?></a></span>
-<i class="fa fa-user-circle"></i><br />
+<i class="fa fa-user"></i><br />
 </div>
 <div>
 <span class="entry-date"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO') ?>"><?php the_time( get_option( 'date_format' ) ); ?></abbr></span>
--- functions.php
+++ functions.php
@@ -19,6 +19,8 @@ function trina_scripts() {
 
 	wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/css/font-awesome.css', array(), 'v4.7.0', false );
 
+	wp_enqueue_style( 'trina-fonts', zillah_fonts_url(), array(), null );
+
 	/*if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 
 		wp_enqueue_script( 'comment-reply' );
@@ -37,6 +39,23 @@ function trina_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'trina_scripts' );
 
+function zillah_fonts_url() {
+	$fonts_url = '';
+
+	$font_families = array();
+
+	$font_families[] = 'Muli:100,200,300,400,500,600,700,800,900';
+
+	$font_families[] = 'Quicksand:100,200,300,400,500,600,700,800,900';
+
+	$query_args = array(
+		'family' => urlencode( implode( '|', $font_families ) ),
+		'subset' => urlencode( 'latin,latin-ext' ),
+	);
+	$fonts_url = add_query_arg( $query_args, '//fonts.googleapis.com/css' );
+
+	return $fonts_url;
+}
 
 add_action('comment_form_before', 'trina_enqueue_comment_reply_script');
 
