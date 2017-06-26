155ec46b35f538ca5e994569bf61f0f2af3dd49b
xTrinch <mojca.rojko@gmail.com>
2017-06-26 09:18:53 +0200

    Fix screenshot dimensions

--- footer.php
+++ footer.php
@@ -2,10 +2,30 @@
 </div>
 <footer>
 <div id="copyright">
-<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'trina' ), '&copy;', date('Y'), esc_html(get_bloginfo('name')) ); ?>
+	<div>
+		<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'trina' ), '&copy;', date_i18n(__('Y','textdomain')), esc_html(get_bloginfo('name')) ); ?>
+	</div>
+	<div>
+		<?php
+		/* translators: 1: Link to WordPress.org */
+		printf( __( 'Proudly powered by %1$s', 'trina' ),
+			/* translators: 1: s: 'WordPress' */
+			sprintf( '<a href="http://wordpress.org/" rel="nofollow">%s</a>', esc_html__( 'WordPress', 'trina' ) )
+		); ?>
+
+		<span class="sep"> | </span>
+
+		<?php
+		/* translators: 1: Link to trina.si */
+		printf( __( 'Theme by %1$s', 'trina' ),
+			/* translators: s: 'Themeisle' */
+			sprintf( '<a href="http://trina.si/" rel="nofollow">%s</a>', esc_html__( 'Trina', 'trina' ) )
+		);
+		?>
+	</div>
 </div>
 </footer>
 </div>
-<?php wp_footer(); ?>
+	<?php wp_footer(); ?>
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -1,65 +1,74 @@
 <?php
 add_action('after_setup_theme', 'trina_setup');
 function trina_setup(){
-load_theme_textdomain('trina', get_template_directory() . '/languages');
-add_theme_support( 'title-tag' );
-add_theme_support( 'custom-header', $args );
-add_theme_support( 'custom-background', $args );
-add_theme_support( 'automatic-feed-links' );
-add_theme_support( 'post-thumbnails' );
-global $content_width;
-if ( ! isset( $content_width ) ) $content_width = 640;
-register_nav_menus(
-array( 'main-menu' => __( 'Main Menu', 'trina' ) )
-);
+	load_theme_textdomain('trina', get_template_directory() . '/languages');
+	add_theme_support( 'title-tag' );
+	add_theme_support( 'custom-header' );
+	add_theme_support( 'custom-background' );
+	add_theme_support( 'automatic-feed-links' );
+	add_theme_support( 'post-thumbnails' );
+	global $content_width;
+	if ( ! isset( $content_width ) ) $content_width = 640;
+	register_nav_menus(
+	array( 'main-menu' => __( 'Main Menu', 'trina' ) )
+	);
 }
+
 add_action('comment_form_before', 'trina_enqueue_comment_reply_script');
-function trina_enqueue_comment_reply_script()
-{
-if(get_option('thread_comments')) { wp_enqueue_script('comment-reply'); }
+
+function trina_enqueue_comment_reply_script() {
+	if(get_option('thread_comments')) { wp_enqueue_script('comment-reply'); }
 }
+
 add_filter('the_title', 'trina_title');
+
 function trina_title($title) {
-if ($title == '') {
-return 'Untitled';
-} else {
-return $title;
-}
+	if ($title == '') {
+	return 'Untitled';
+	} else {
+	return $title;
+	}
 }
 add_filter('wp_title', 'trina_filter_wp_title');
-function trina_filter_wp_title($title)
-{
-return $title . esc_attr(get_bloginfo('name'));
+
+function trina_filter_wp_title($title) {
+	return $title . esc_attr(get_bloginfo('name'));
 }
+
 add_filter('comment_form_defaults', 'trina_comment_form_defaults');
-function trina_comment_form_defaults( $args )
-{
-$req = get_option( 'require_name_email' );
-$required_text = sprintf( ' ' . __('Required fields are marked %s', 'trina'), '<span class="required">*</span>' );
-$args['comment_notes_before'] = '<p class="comment-notes">' . __('Your email is kept private.', 'trina') . ( $req ? $required_text : '' ) . '</p>';
-$args['title_reply'] = __('', 'trina');
-$args['title_reply_to'] = __('Post a Reply to %s', 'trina');
-return $args;
+
+function trina_comment_form_defaults( $args ) {
+	$req = get_option( 'require_name_email' );
+	$required_text = sprintf( ' ' . __('Required fields are marked %s', 'trina'), '<span class="required">*</span>' );
+	$args['comment_notes_before'] = '<p class="comment-notes">' . __('Your email is kept private.', 'trina') . ( $req ? $required_text : '' ) . '</p>';
+	$args['title_reply'] = __('', 'trina');
+	$args['title_reply_to'] = __('Post a Reply to %s', 'trina');
+	return $args;
 }
+
 add_action( 'widgets_init', 'trina_widgets_init' );
+
 function trina_widgets_init() {
-register_sidebar( array (
-'name' => __('Sidebar Widget Area', 'trina'),
-'id' => 'primary-widget-area',
-'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
-'after_widget' => "</li>",
-'before_title' => '<h3 class="widget-title">',
-'after_title' => '</h3>',
-) );
+	register_sidebar( array (
+	'name' => __('Sidebar Widget Area', 'trina'),
+	'id' => 'primary-widget-area',
+	'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
+	'after_widget' => "</li>",
+	'before_title' => '<h3 class="widget-title">',
+	'after_title' => '</h3>',
+	) );
 }
+
 $preset_widgets = array (
 'primary-aside'  => array( 'search', 'pages', 'categories', 'archives' ),
 );
+
 function trina_get_page_number() {
-if (get_query_var('paged')) {
-print ' | ' . __( 'Page ' , 'trina') . get_query_var('paged');
+	if (get_query_var('paged')) {
+	print ' | ' . __( 'Page ' , 'trina') . get_query_var('paged');
+	}
 }
-}
+
 function trina_catz($glue) {
 $current_cat = single_cat_title( '', false );
 $separator = "\n";
@@ -95,64 +104,66 @@ $avatar_email = get_comment_author_email();
 $avatar = str_replace( "class='avatar", "class='photo avatar", get_avatar( $avatar_email, 80 ) );
 echo $avatar . ' <span class="fn n">' . $commenter . '</span>';
 }
-function trina_custom_comments($comment, $args, $depth) {
-$GLOBALS['comment'] = $comment;
-$GLOBALS['comment_depth'] = $depth;
-?>
-<li id="comment-<?php comment_ID() ?>" <?php comment_class() ?>>
-<div class="comment-author vcard"><?php trina_commenter_link() ?></div>
-<div class="comment-meta"><?php printf(__('Posted %1$s at %2$s', 'trina' ), get_comment_date(), get_comment_time() ); ?><span class="meta-sep"> | </span> <a href="#comment-<?php echo get_comment_ID(); ?>" title="<?php _e('Permalink to this comment', 'trina' ); ?>"><?php _e('Permalink', 'trina' ); ?></a>
-<?php edit_comment_link(__('Edit', 'trina'), ' <span class="meta-sep"> | </span> <span class="edit-link">', '</span>'); ?></div>
-<?php if ($comment->comment_approved == '0') { echo '\t\t\t\t\t<span class="unapproved">'; _e('Your comment is awaiting moderation.', 'trina'); echo '</span>\n'; } ?>
-<div class="comment-content">
-<?php comment_text() ?>
-</div>
-<?php
-if($args['type'] == 'all' || get_comment_type() == 'comment') :
-comment_reply_link(array_merge($args, array(
-'reply_text' => __('Reply','trina'),
-'login_text' => __('Login to reply.', 'trina'),
-'depth' => $depth,
-'before' => '<div class="comment-reply-link">',
-'after' => '</div>'
-)));
-endif;
-?>
-<?php }
-function trina_custom_pings($comment, $args, $depth) {
-$GLOBALS['comment'] = $comment;
-?>
-<li id="comment-<?php comment_ID() ?>" <?php comment_class() ?>>
-<div class="comment-author"><?php printf(__('By %1$s on %2$s at %3$s', 'trina'),
-get_comment_author_link(),
-get_comment_date(),
-get_comment_time() );
-edit_comment_link(__('Edit', 'trina'), ' <span class="meta-sep"> | </span> <span class="edit-link">', '</span>'); ?></div>
-<?php if ($comment->comment_approved == '0') { echo '\t\t\t\t\t<span class="unapproved">'; _e('Your trackback is awaiting moderation.', 'trina'); echo '</span>\n'; } ?>
-<div class="comment-content">
-<?php comment_text() ?>
-</div>
-<?php }
 
+function trina_custom_comments($comment, $args, $depth) {
+	$GLOBALS['comment'] = $comment;
+	$GLOBALS['comment_depth'] = $depth;
+	?>
+	<li id="comment-<?php comment_ID() ?>" <?php comment_class() ?>>
+	<div class="comment-author vcard"><?php trina_commenter_link() ?></div>
+	<div class="comment-meta"><?php printf(__('Posted %1$s at %2$s', 'trina' ), get_comment_date(), get_comment_time() ); ?><span class="meta-sep"> | </span> <a href="#comment-<?php echo get_comment_ID(); ?>" title="<?php _e('Permalink to this comment', 'trina' ); ?>"><?php _e('Permalink', 'trina' ); ?></a>
+	<?php edit_comment_link(__('Edit', 'trina'), ' <span class="meta-sep"> | </span> <span class="edit-link">', '</span>'); ?></div>
+	<?php if ($comment->comment_approved == '0') { echo '\t\t\t\t\t<span class="unapproved">'; _e('Your comment is awaiting moderation.', 'trina'); echo '</span>\n'; } ?>
+	<div class="comment-content">
+	<?php comment_text() ?>
+	</div>
+	<?php
+	if($args['type'] == 'all' || get_comment_type() == 'comment') :
+	comment_reply_link(array_merge($args, array(
+	'reply_text' => __('Reply','trina'),
+	'login_text' => __('Login to reply.', 'trina'),
+	'depth' => $depth,
+	'before' => '<div class="comment-reply-link">',
+	'after' => '</div>'
+	)));
+	endif;
+}
+
+function trina_custom_pings($comment, $args, $depth) {
+	$GLOBALS['comment'] = $comment;
+	?>
+	<li id="comment-<?php comment_ID() ?>" <?php comment_class() ?>>
+	<div class="comment-author"><?php printf(__('By %1$s on %2$s at %3$s', 'trina'),
+	get_comment_author_link(),
+	get_comment_date(),
+	get_comment_time() );
+	edit_comment_link(__('Edit', 'trina'), ' <span class="meta-sep"> | </span> <span class="edit-link">', '</span>'); ?></div>
+	<?php if ($comment->comment_approved == '0') { echo '\t\t\t\t\t<span class="unapproved">'; _e('Your trackback is awaiting moderation.', 'trina'); echo '</span>\n'; } ?>
+	<div class="comment-content">
+	<?php comment_text() ?>
+	</div>
+	<?php
+}
 
 function trina_read_more_link() {
 	/* translators: s: The post title */
 	return '<a href="' . esc_url( get_permalink( get_the_ID() ) ) . '" class="more-link">' . sprintf( __( 'Continue Reading %s', 'trina' ), the_title( '<span class="screen-reader-text">"', '"</span>', false ) . ' &nbsp;<i class="fa fa-angle-right"></i>' ) . '</a>';
 }
+
 add_filter( 'the_content_more_link', 'trina_read_more_link' );
 
 function trina_custom_excerpt_length( $length ) {
     return 70;
 }
-add_filter( 'excerpt_length', 'trina_custom_excerpt_length', 999 );
 
+add_filter( 'excerpt_length', 'trina_custom_excerpt_length', 999 );
 
 function trina_allowedtags() {
     // Add custom tags to this string
-        return '<script>,<style>,<br>,<em>,<i>,<ul>,<ol>,<li>,<a>,<p>,<img>,<video>,<audio>,<code>,<ins>'; 
+        return '<script>,<style>,<br>,<em>,<i>,<ul>,<ol>,<li>,<a>,<p>,<img>,<video>,<audio>,<code>,<ins>';
     }
 
-if ( ! function_exists( 'trina_custom_wp_trim_excerpt' ) ) : 
+if ( ! function_exists( 'trina_custom_wp_trim_excerpt' ) ) :
 
     function trina_custom_wp_trim_excerpt($trina_excerpt) {
     $raw_excerpt = $trina_excerpt;
@@ -166,7 +177,7 @@ if ( ! function_exists( 'trina_custom_wp_trim_excerpt' ) ) :
 
             //Set the excerpt word count and only break after sentence is complete.
                 $excerpt_word_count = 70;
-                $excerpt_length = apply_filters('excerpt_length', $excerpt_word_count); 
+                $excerpt_length = apply_filters('excerpt_length', $excerpt_word_count);
                 $tokens = array();
                 $excerptOutput = '';
                 $count = 0;
@@ -174,9 +185,9 @@ if ( ! function_exists( 'trina_custom_wp_trim_excerpt' ) ) :
                 // Divide the string into tokens; HTML tags, or words, followed by any whitespace
                 preg_match_all('/(<[^>]+>|[^<>\s]+)\s*/u', $trina_excerpt, $tokens);
 
-                foreach ($tokens[0] as $token) { 
+                foreach ($tokens[0] as $token) {
 
-                    if ($count >= $excerpt_length && preg_match('/[\,\;\?\.\!]\s*$/uS', $token)) { 
+                    if ($count >= $excerpt_length && preg_match('/[\,\;\?\.\!]\s*$/uS', $token)) {
                     // Limit reached, continue until , ; ? . or ! occur at the end
                         $excerptOutput .= trim($token);
                         break;
@@ -191,8 +202,8 @@ if ( ! function_exists( 'trina_custom_wp_trim_excerpt' ) ) :
 
             $trina_excerpt = trim(force_balance_tags($excerptOutput));
 
-                $excerpt_end = ' <a href="'. esc_url( get_permalink() ) . '">' . '&nbsp;&raquo;&nbsp;' . sprintf(__( 'Read more about: %s &nbsp;&raquo;', 'trina' ), get_the_title()) . '</a>'; 
-                $excerpt_more = apply_filters('excerpt_more', ' ' . $excerpt_end); 
+                $excerpt_end = ' <a href="'. esc_url( get_permalink() ) . '">' . '&nbsp;&raquo;&nbsp;' . sprintf(__( 'Read more about: %s &nbsp;&raquo;', 'trina' ), get_the_title()) . '</a>';
+                $excerpt_more = apply_filters('excerpt_more', ' ' . $excerpt_end);
 
                 //$pos = strrpos($trina_excerpt, '</');
                 //if ($pos !== false)
@@ -202,18 +213,18 @@ if ( ! function_exists( 'trina_custom_wp_trim_excerpt' ) ) :
                 // After the content
                 // $trina_excerpt .= $excerpt_more; /*Add read more in new paragraph */
 
-            return $trina_excerpt;   
+            return $trina_excerpt;
 
         }
         return apply_filters('trina_custom_wp_trim_excerpt', $trina_excerpt, $raw_excerpt);
     }
 
-endif; 
+endif;
 
 remove_filter('get_the_excerpt', 'wp_trim_excerpt');
-add_filter('get_the_excerpt', 'trina_custom_wp_trim_excerpt'); 
+add_filter('get_the_excerpt', 'trina_custom_wp_trim_excerpt');
 
 add_filter( 'wp_postratings_ratings_image_alt', 'wp_postratings_ratings_image_alt' );
 function wp_postratings_ratings_image_alt( $alt_title_text ) {
-return '';
+	return '';
 }
